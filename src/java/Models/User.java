6f7c1c297c8cad977bf1b8c5e5b6a2b76c27bdbf
850354 <850354@SAIT227242IT.ACDM.DS.SAIT.CA>
2021-06-20 17:24:04 -0600

    User getters and seters, enforced values for users created the java class named user to get the username and password as the assign mentioned we have to pretend that a user has signed up already so we jus log them in in this case we have adam and betty.

--- src/java/Models/User.java
+++ src/java/Models/User.java
@@ -10,5 +10,31 @@ package Models;
  * @author 850354
  */
 public class User {
+    //in this class i want to get the user's input and set
+    //thread safety remember
+    private String username;
+    private String password;
+    
+    public User(String username, String password){
+      this.username =username;
+      this.password =password;
+    }
+    
+    public String getUsername (){
+            return username;
+    }
+    
+    public void setUsername (String username){
+        this.username = username;
+    }
+    
+    public String getPassword(){
+        return password;
+    }
+    public void setPassword (String password){
+        this.password = password;
+    }
     
 }
+    
+
--- src/java/Service/AccountService.java
+++ src/java/Service/AccountService.java
@@ -1,14 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Service;
+
+import Models.User;
 
 /**
  *
  * @author 850354
  */
 public class AccountService {
+    //here we enforce the user to input the two
+    //names we want for testing, like they already signed up
+    //now they are loging in with the info 
     
+    public User login(String username, String password){
+        //here is how we enforce user input with names adam and betty with pass 
+        //of password
+        
+        if ((username.equals("betty") || username.equals("adam")) && password.equals("password")){
+            //creates a new user
+            return new User (username);
+        } 
+        
+        return null;
+    }
 }
